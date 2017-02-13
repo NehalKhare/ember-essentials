61bb71686195f963ac3442578c735d57be462305
Micah Bales <micahbales@gmail.com>
2017-02-13 14:11:13 -0500

    add nested routes to bookmarks

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.route('bookmarks');
+  this.route('bookmarks', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
