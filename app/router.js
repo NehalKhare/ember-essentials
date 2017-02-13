8c654a3c464a6cc162c7b3707e9e7790c19792ad
Micah Bales <micahbales@gmail.com>
2017-02-13 14:18:52 -0500

    add dynamic routes and wildcard route

--- app/router.js
+++ app/router.js
@@ -9,6 +9,14 @@ Router.map(function() {
   this.route('about');
   this.route('bookmarks', function() {
     this.route('new');
+
+    this.route('edit', {
+      path: '/edit/:bookmark_id'
+    });
+  });
+
+  this.route('page-not-found', {
+    path: '/*wildcard'
   });
 });
 
