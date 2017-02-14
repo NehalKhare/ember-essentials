14b713bf4a189fa5ff852b26655f47104f6d731a
Micah Bales <micahbales@gmail.com>
2017-02-14 13:37:35 -0500

    configure dev environment & add title to ENV.APP

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
+import ENV from 'bookmarker/config/environment'
 
 export default Ember.Route.extend({
+  model() {
+    Ember.Logger.info("ENV.APP.title === " + ENV.APP.title);
+  }
 });
